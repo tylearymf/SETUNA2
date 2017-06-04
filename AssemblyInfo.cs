208547690f5e8c37b80e4792ff7c314b3be53f54
tylearymf <tylearymf@163.com>
2017-06-04 16:17:08 +0800

    添加Icon、修改版本号

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿// Assembly SETUNA, Version 2.2.1.0
+﻿// Assembly SETUNA, Version 2.3
 
 [assembly: System.Reflection.AssemblyTitle("SETUNA")]
 [assembly: System.Reflection.AssemblyDescription("")]
@@ -9,7 +9,7 @@
 [assembly: System.Runtime.InteropServices.ComVisible(false)]
 [assembly: System.Runtime.InteropServices.Guid("4483e561-8b3e-427d-98a4-e0e821b7bf2f")]
 [assembly: System.Reflection.AssemblyConfiguration("")]
-[assembly: System.Reflection.AssemblyFileVersion("2.2.1.0")]
+[assembly: System.Reflection.AssemblyFileVersion("2.3")]
 [assembly: System.Runtime.CompilerServices.CompilationRelaxations(8)]
 [assembly: System.Runtime.CompilerServices.RuntimeCompatibility(WrapNonExceptionThrows=true)]
 
