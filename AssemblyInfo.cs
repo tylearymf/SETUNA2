98f7e540bc3fbfbbdec2810892963f6f1656abb9
tylearymf <tylearymf@163.com>
2018-03-24 23:47:42 +0800

    Enable DPI

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -1,7 +1,7 @@
-﻿// Assembly SETUNA, Version 2.3
+﻿// Assembly SETUNA, Version 2.4
 
 [assembly: System.Reflection.AssemblyTitle("SETUNA")]
-[assembly: System.Reflection.AssemblyDescription("")]
+[assembly: System.Reflection.AssemblyDescription("https://github.com/tylearymf/SETUNA2")]
 [assembly: System.Reflection.AssemblyProduct("SETUNA")]
 [assembly: System.Reflection.AssemblyCopyright("Copyright (C) 2008 CLEARUP")]
 [assembly: System.Reflection.AssemblyTrademark("")]
@@ -9,7 +9,7 @@
 [assembly: System.Runtime.InteropServices.ComVisible(false)]
 [assembly: System.Runtime.InteropServices.Guid("4483e561-8b3e-427d-98a4-e0e821b7bf2f")]
 [assembly: System.Reflection.AssemblyConfiguration("")]
-[assembly: System.Reflection.AssemblyFileVersion("2.3")]
+[assembly: System.Reflection.AssemblyFileVersion("2.4")]
 [assembly: System.Runtime.CompilerServices.CompilationRelaxations(8)]
 [assembly: System.Runtime.CompilerServices.RuntimeCompatibility(WrapNonExceptionThrows=true)]
 
--- SETUNA/Main/SplashForm.cs
+++ SETUNA/Main/SplashForm.cs
@@ -31,61 +31,80 @@
 
         private void InitializeComponent()
         {
-            this.components = new Container();
-            this.SplashTimer = new Timer(this.components);
-            this.pictureBox1 = new PictureBox();
-            this.label1 = new Label();
-            this.lblVer = new Label();
-            ((ISupportInitialize) this.pictureBox1).BeginInit();
-            base.SuspendLayout();
-            this.SplashTimer.Interval = 0xbb8;
-            this.SplashTimer.Tick += new EventHandler(this.SplashTimer_Tick);
-            this.pictureBox1.Image = Resources.Logo;
+            this.components = new System.ComponentModel.Container();
+            this.SplashTimer = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblVer = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // SplashTimer
+            // 
+            this.SplashTimer.Interval = 3000;
+            this.SplashTimer.Tick += new System.EventHandler(this.SplashTimer_Tick);
+            // 
+            // pictureBox1
+            // 
             this.pictureBox1.InitialImage = null;
-            this.pictureBox1.Location = new Point(-1, -1);
-            this.pictureBox1.Margin = new Padding(0);
+            this.pictureBox1.Location = new System.Drawing.Point(-2, -2);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(0);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new Size(400, 0x7e);
-            this.pictureBox1.SizeMode = PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.Size = new System.Drawing.Size(989, 298);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
-            this.pictureBox1.Click += new EventHandler(this.pictureBox1_Click);
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.pictureBox1.Image = Resources.Logo;
+            // 
+            // label1
+            // 
             this.label1.AutoSize = true;
-            this.label1.Cursor = Cursors.Hand;
-            this.label1.ForeColor = Color.Gray;
-            this.label1.Location = new Point(0x51, 150);
+            this.label1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(202, 375);
+            this.label1.Margin = new System.Windows.Forms.Padding(8, 0, 8, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new Size(0xe3, 12);
+            this.label1.Size = new System.Drawing.Size(553, 30);
             this.label1.TabIndex = 1;
-            this.label1.Text = "http://www.clearunit.com/clearup/setuna2/";
-            this.lblVer.ForeColor = Color.Gray;
-            this.lblVer.Location = new Point(12, 0x81);
+            this.label1.Text = "https://github.com/tylearymf/SETUNA2";
+            // 
+            // lblVer
+            // 
+            this.lblVer.ForeColor = System.Drawing.Color.Gray;
+            this.lblVer.Location = new System.Drawing.Point(30, 322);
+            this.lblVer.Margin = new System.Windows.Forms.Padding(8, 0, 8, 0);
             this.lblVer.Name = "lblVer";
-            this.lblVer.Size = new Size(0x16e, 0x12);
+            this.lblVer.Size = new System.Drawing.Size(915, 45);
             this.lblVer.TabIndex = 4;
             this.lblVer.Text = "SETUNA 2.0α";
-            this.lblVer.TextAlign = ContentAlignment.MiddleCenter;
-            base.AutoScaleDimensions = new SizeF(6f, 12f);
-            base.AutoScaleMode = AutoScaleMode.Font;
-            this.BackColor = Color.White;
-            base.ClientSize = new Size(0x18d, 0xa2);
-            base.ControlBox = false;
-            base.Controls.Add(this.lblVer);
-            base.Controls.Add(this.label1);
-            base.Controls.Add(this.pictureBox1);
+            this.lblVer.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // SplashForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(15F, 30F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(982, 425);
+            this.ControlBox = false;
+            this.Controls.Add(this.lblVer);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
             this.DoubleBuffered = true;
-            base.FormBorderStyle = FormBorderStyle.FixedSingle;
-            base.MaximizeBox = false;
-            base.MinimizeBox = false;
-            base.Name = "SplashForm";
-            base.ShowIcon = false;
-            base.ShowInTaskbar = false;
-            base.StartPosition = FormStartPosition.CenterScreen;
-            base.TopMost = true;
-            base.Load += new EventHandler(this.SplashForm_Load);
-            ((ISupportInitialize) this.pictureBox1).EndInit();
-            base.ResumeLayout(false);
-            base.PerformLayout();
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(8, 8, 8, 8);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "SplashForm";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.SplashForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         private void pictureBox1_Click(object sender, EventArgs e)
